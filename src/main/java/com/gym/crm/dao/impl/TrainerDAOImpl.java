00dda99c7abf44cf8089c2714794de981ad4ecdb
juliakhomyn <jkhomyn1381@gmail.com>
2026-04-19 13:38:44 +0300

    GIA-46 Add user profile functionality - add validator and id generation

--- src/main/java/com/gym/crm/dao/impl/TrainerDAOImpl.java
+++ src/main/java/com/gym/crm/dao/impl/TrainerDAOImpl.java
@@ -4,6 +4,7 @@ import com.gym.crm.dao.TrainerDAO;
 import com.gym.crm.model.Trainer;
 import com.gym.crm.model.enums.StorageNamespace;
 import com.gym.crm.storage.InMemoryStorage;
+import com.gym.crm.util.Validator;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,29 +15,29 @@ import java.util.Optional;
 
 @Repository
 public class TrainerDAOImpl implements TrainerDAO {
-    private static final String INVALID_ID_EXCEPTION_MESSAGE = "ID must be positive and not null, got: %s";
 
     @Setter(onMethod_={@Autowired})
     private InMemoryStorage inMemoryStorage;
 
     @Override
     public Trainer save(Trainer trainer) {
-        return trainerStorage().put(trainer.getUserId(), trainer);
+        Trainer toSave = trainer.getUserId() == null
+                ? trainer.toBuilder().userId(generateId()).build()
+                : trainer;
+        trainerStorage().put(toSave.getUserId(), toSave);
+        return toSave;
     }
 
     @Override
     public Trainer update(Trainer trainer) {
-        if (trainer.getUserId() == null || trainer.getUserId() <= 0) {
-            throw new IllegalArgumentException(String.format(INVALID_ID_EXCEPTION_MESSAGE, trainer.getUserId()));
-        }
-        return trainerStorage().put(trainer.getUserId(), trainer);
+        Validator.validateId(trainer.getUserId());
+        trainerStorage().put(trainer.getUserId(), trainer);
+        return trainer;
     }
 
     @Override
     public Optional<Trainer> findById(Long id) {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException(String.format(INVALID_ID_EXCEPTION_MESSAGE, id));
-        }
+        Validator.validateId(id);
         return Optional.ofNullable(trainerStorage().get(id));
     }
 
@@ -47,5 +48,9 @@ public class TrainerDAOImpl implements TrainerDAO {
 
     private Map<Long, Trainer> trainerStorage() {
         return (Map<Long, Trainer>) inMemoryStorage.getStorage(StorageNamespace.TRAINER);
+    }
+
+    private long generateId() {
+        return trainerStorage().keySet().stream().max(Long::compareTo).orElse(0L) + 1;
     }
 }

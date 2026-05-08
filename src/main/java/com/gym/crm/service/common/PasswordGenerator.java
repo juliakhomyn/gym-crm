9bb9d1f8cbe30bce4d2d25bfe504ca8736338783
juliakhomyn <jkhomyn1381@gmail.com>
2026-05-08 17:05:43 +0300

    GIA-97 Setup CI wih Sonarqube - fix sonar setup

--- src/main/java/com/gym/crm/service/common/PasswordGenerator.java
+++ src/main/java/com/gym/crm/service/common/PasswordGenerator.java
@@ -2,6 +2,8 @@ package com.gym.crm.service.common;
 
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+
 @Component
 public class PasswordGenerator {
     private static final int PASSWORD_LENGTH = 10;
@@ -9,11 +11,13 @@ public class PasswordGenerator {
             + "abcdefghijklmnopqrstuvxyz"
             + "0123456789";
 
+    private final SecureRandom random = new SecureRandom();
+
     public String generatePassword() {
         StringBuilder password = new StringBuilder();
 
         for (int i = 0; i < PASSWORD_LENGTH; i++) {
-            int index = (int) (Math.random() * CHARS.length());
+            int index = random.nextInt(CHARS.length());
             password.append(CHARS.charAt(index));
         }
 

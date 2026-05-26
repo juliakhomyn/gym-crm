3bdbadbf1af41f77ef3dd7d3de6fa48c27818eb4
juliakhomyn <jkhomyn1381@gmail.com>
2026-05-26 13:46:12 +0300

    GIA-164 Migrate to Spring Boot - remove unused code

--- src/test/java/com/gym/crm/testutils/TestDataProvider.java
+++ src/test/java/com/gym/crm/testutils/TestDataProvider.java
@@ -571,31 +571,9 @@ public class TestDataProvider {
                 .build();
     }
 
-    public static ToggleActiveRequestDTO buildInvalidToggleActiveRequest() {
-        return ToggleActiveRequestDTO.builder()
-                .username(" ")
-                .build();
-    }
-
     public static ToggleActiveRequestDTO buildToggleActiveRequestNonExistent() {
         return ToggleActiveRequestDTO.builder()
                 .username(NOT_FOUND_USERNAME)
-                .build();
-    }
-
-    public static PasswordChangeRequest buildPasswordChangeRequestBlankUsername() {
-        return PasswordChangeRequest.builder()
-                .username(" ")
-                .oldPassword(PASSWORD)
-                .newPassword(NEW_PASSWORD)
-                .build();
-    }
-
-    public static PasswordChangeRequest buildPasswordChangeRequestShortNewPassword() {
-        return PasswordChangeRequest.builder()
-                .username(USERNAME)
-                .oldPassword(PASSWORD)
-                .newPassword("short")
                 .build();
     }
 

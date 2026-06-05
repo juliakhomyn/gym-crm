8d5646da856ca390bd61c8b9404517c4ada8efcd
juliakhomyn <jkhomyn1381@gmail.com>
2026-06-05 16:21:06 +0200

    GIA-200 Configure authorization access rules - remove unused import

--- src/test/java/com/gym/crm/controller/AuthControllerTest.java
+++ src/test/java/com/gym/crm/controller/AuthControllerTest.java
@@ -28,7 +28,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static com.gym.crm.utils.JsonUtil.readJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;

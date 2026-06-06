815c9859baa2bc9bd8ad2eaead8888f5f4680070
juliakhomyn <jkhomyn1381@gmail.com>
2026-06-06 11:09:11 +0200

    GIA-201 Implement brute force protection - remove unused import

--- src/main/java/com/gym/crm/service/common/AuthenticationService.java
+++ src/main/java/com/gym/crm/service/common/AuthenticationService.java
@@ -15,7 +15,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
